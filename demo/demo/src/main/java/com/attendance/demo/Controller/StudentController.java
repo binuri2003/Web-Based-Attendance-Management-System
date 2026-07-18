440128b18820eb819101376cb4d3bc5170e9ed77
binuri2003 <[binuri.vihangi2003@gmail.com](mailto:binuri.vihangi2003@gmail.com)>
2026-07-18 20:11:17 +0530

    Success login backend with polymorphism

--- demo/demo/src/main/java/com/attendance/demo/Controller/StudentController.java
+++ demo/demo/src/main/java/com/attendance/demo/Controller/StudentController.java
@@ -1,5 +1,14 @@
 package com.attendance.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StudentController {
-    
+
+    @GetMapping("/student/dashboard")
+    public String studentDashboard() {
+        return "student/dashboard";
+    }
+
 }
---
+++ demo/demo/src/main/java/com/attendance/demo/entity/Role.java
@@ -0,0 +1,9 @@
+package com.attendance.demo.entity;
+
+public enum Role {
+
+    Admin,
+    Lecturer,
+    Student
+
+}
